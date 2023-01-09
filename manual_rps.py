e3d1bb7fda0e60794de971efb55eaa3ffb270063
shirish-verma <shirishverma@gmail.com>
2023-01-09 22:41:17 +0000

    Corrections to m4, task 1.

--- manual_rps.py
+++ manual_rps.py
@@ -3,20 +3,16 @@ import random
 rps_list = ['rock', 'paper', 'scissors']
 
 def get_computer_choice():
-    rps_choices = ['rock', 'paper', 'scissors']
+    rps_choices = ['Rock', 'Paper', 'Scissors']
     computer_choice = random.choice(rps_choices)
     return computer_choice
 
 def get_user_choice():
     rps_choices = ['rock', 'paper', 'scissors']
     while True:
-        user_choice = input("Player 1: Rock, Paper or Scissors? >> ").lower()
+        user_choice = input("Player 1: Rock, Paper or Scissors? >> ").title()
         if user_choice in rps_choices:
             return user_choice
         else:
             print('Invalid input. Please enter either rock, paper or scissors. Input is not case sensitive.')
-
-
-print(get_user_choice())
-print(get_computer_choice())
 
