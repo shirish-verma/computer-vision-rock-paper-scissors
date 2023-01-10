7960ae065909bdf788a380f0128934a9dfb80b09
shirish-verma <shirishverma@gmail.com>
2023-01-10 04:26:07 +0000

    Adding m5, task 3.

--- camera_rps.py
+++ camera_rps.py
@@ -8,8 +8,11 @@ cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 def get_prediction():
+    start_time = time.time()
+    elasped_time = time.time() - start_time
     pred_list = []
-    while True: 
+    countdown = 2
+    while (countdown - elasped_time) > 0: 
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
@@ -17,7 +20,8 @@ def get_prediction():
         data[0] = normalized_image
         prediction = model.predict(data)
         cv2.imshow('frame', frame)
-        pred_list.append(np.argmax(prediction))
+        pred_list.append(np.argmax(prediction))    
+        elasped_time = time.time() - start_time
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break    
     cap.release() # After the loop release the cap object
@@ -39,20 +43,36 @@ def get_computer_choice():
 def get_winner(computer_choice, user_choice):
     if user_choice == 'Nothing':
         print('You didnt make a choice. Round forfeited.')
+        return 1
     elif computer_choice == user_choice:
         print('It is a tie!')
+        return 0
     elif (computer_choice == 'Rock' and user_choice == 'Scissors') or (computer_choice == 'Paper' and user_choice == 'Rock') or (computer_choice == 'Scissors' and user_choice == 'Paper'):
         print('You lost')
+        return 1
     else:
         print('You won!')
+        return 2
 
 def play():
-    computer = get_computer_choice()
     user = get_user_choice()
+    computer = get_computer_choice()
     get_winner(computer, user)
 
-seconds = time.time()
+def rps_match():
+    computer_wins = 0
+    user_wins = 0
+    for round in range(2):
+        round_result = play()
+        if round_result == 1:
+            computer_wins += 1
+        elif round_result == 2:
+            user_wins += 1
+        elif (computer_wins or user_wins) == 3:
+            print(f'Computer: {computer_wins}, User: {user_wins}')
+            break
+        print(f'End of round {round + 1}')
+    print('Game over. All 5 rounds have been played!')
+    print(f'Computer: {computer_wins}, User: {user_wins}')
 
-get_prediction()
-
-
+rps_match()
